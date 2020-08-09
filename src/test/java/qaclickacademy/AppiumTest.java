c100bc3dfe3371f5e3dfd3cb1f0c90bc350118c8
sandipan <sandipanb@MPTDC1-India.MirrorPlus.com>
2020-08-10 02:00:40 +0530

    1st commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidTest()
+	{
+		System.out.println("Testing Android Device");
+	}
+	
+	@Test
+	public void iOSTest()
+	{
+		System.out.println("Testing iOS Device");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RestapiTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestapiTest {
+
+	@Test	
+	public void JiraTest()
+	{
+		System.out.println("Testing Jira Tool");
+	}
+
+	@Test	
+	public void TwitterApi()
+	{
+		System.out.println("Testing API in Twitter");
+		
+	}
+}
